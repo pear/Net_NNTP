1296451d90ea0c5cc9681904b0fb58930fdaae2e
Heino H. Gehlsen <heino@php.net>
2005-11-27 21:49:10 +0000

    Rewrite
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_NNTP/branches/MAINT_1_2@201450 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/news.php.net/group.php
+++ docs/examples/news.php.net/group.php
@@ -18,61 +18,80 @@
 // +----------------------------------------------------------------------+
 //
 // $Id$
+
+$host = 'news.php.net';
+$debug = false;
+
 ?>
 <html>
+
 <head>
-    <title>NNTP news.php.net</title>
+    <title>NNTP <?php echo $host; ?></title>
 </head>
+
 <body>
 <?php
+
+if (!isset($_GET['group'])) {
+    die('<font color="red">No newsgroup choosed!</font><br>');
+}
+
+$group = $_GET['group'];
+
 require_once "Net/NNTP/Client.php";
 
 $nntp = new Net_NNTP_Client();
+$nntp->setDebug($debug);
 
-$ret = $nntp->connect("news.php.net");
-if( PEAR::isError($ret)) {
- echo '<font color="red">No connection to newsserver!</font><br>' ;
- echo $ret->getMessage();
-} else {
-    if(isset($_GET['group'])) {
-        $msgdata = $nntp->selectGroup($_GET['group']);
-        if(PEAR::isError($msgdata)) {
-            echo '<font color="red">'.$msgdata->getMessage().'</font><br>' ;        
-        } else {
-            $msgcount = $msgdata['last']-$msgdata['first'];
-            echo '<h1>'.$_GET['group'].'</h1>';
-            echo "<b>Message count:</b>&nbsp;".$msgcount;
-            echo "<br><b>Posting allowed:</b>&nbsp;";
-            switch( $_GET['writable']) {
-                case 'y' :
-                    echo 'yes';
-                    break;
-                case 'n' :
-                    echo 'no';
-                    break;
-                case 'm' :
-                    echo 'moderated';
-                    break;         
-                default:
-                    echo 'unknown';
-            }
-            echo "<hr>";
-            echo "<h2>last 10 messages</h2>";
+$posting = $nntp->connect($host);
+if (PEAR::isError($posting)) {
+    echo '<font color="red">No connection to newsserver!</font><br>';
+    die ($posting->getMessage());
+}
+
+$currentgroup = $nntp->selectGroup($group);
+if (PEAR::isError($currentgroup)) {
+    echo '<font color="red">' . $currentgroup->getMessage() . '</font><br>';
+}
+
+$messageCount = $currentgroup['last'] - $currentgroup['first'];
+
+echo '<h1>', $group, '</h1>';
+echo '<b>Message count:</b>&nbsp;', $messageCount;
+echo '<br><b>Posting: </b>&nbsp;';
+switch ($_GET['writable']) {
+    case 'y' :
+        echo 'Allowed';
+        break;
+    case 'n' :
+        echo 'Disallowed';
+        break;
+    case 'm' :
+        echo 'Moderated';
+        break;         
+    default:
+        echo 'Unknown';
+}
+echo '<hr>';
+echo '<h2>last 10 messages</h2>';
                 
-            $msgs = array_reverse($nntp->getOverview( $msgcount-9, $msgcount));
-            foreach($msgs as $msgid => $msgheader) {
-                echo '<a href="read.php?msgid='.urlencode($msgid).
-                    '&group='.urlencode($_GET['group']).
-                    '"><b>'.$msgheader["Subject"].'</b></a><br>';
-                echo 'from:&nbsp;'.$msgheader["From"]."<br>";
-                echo $msgheader["Date"].'<br><br>';
-            }        
-        }
-    } else {
-        echo '<font color="red">No newsgroup choosed!</font><br>' ;    
-    }
-    $nntp->quit();
-}    
+$overview = $nntp->getOverview( $messageCount-9, $messageCount);
+if (PEAR::isError($overview)) {
+    die('<font color="red">' . $overview->getMessage() . '</font><br>');
+}
+
+foreach ($overview as $message) {
+    $messageNumber = $message['Number'];
+    $messageID = $message['Message-ID'];
+
+    echo '<a href="read.php?msgid=', urlencode($messageID), '&group=', urlencode($_GET['group']), '"><b>', $message['Subject'], '</b></a><br>';
+    echo 'from:&nbsp;', $message['From'], '<br>';
+    echo $message['Date'], '<br><br>';
+}
+
+$nntp->quit();
+
 ?>
 </body>
+
 </html>
