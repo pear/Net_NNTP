f8ce776dbcc7524d1c66b321ad592a2b42cdc27f
Heino H. Gehlsen <heino@php.net>
2011-08-15 16:00:23 +0000

    Extend license notice to allow inclution in Debian.
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_NNTP/trunk@314976 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/demo/footer.inc.php
+++ docs/examples/demo/footer.inc.php
@@ -3,7 +3,7 @@
  <hr />
 
  <div class="copyright">
-  Copyright &copy; 2011 by Heino H. Gehlsen. All rights reserved.
+  Copyright &copy; 2011 by Heino H. Gehlsen. All rights reserved. This work is distributed under the <a href="http://www.w3.org/Consortium/Legal/2002/copyright-software-20021231">W3C Software License</a> in the hope that it will be useful, but WITHOUT ANY WARRANTY of any kind.
  </div>
 
  <div class="">
